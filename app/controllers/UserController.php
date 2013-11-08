6081d1863562c5250bfb47f84707d1c60229a75f
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-09 04:09:04 +0800

    user table

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends BaseController {
         return Response::json(array('c' => 200, 'm' => 'OK', 'd' => array('email' => $email)));
     }
 
-    public function getProfile() {
+    public function postProfile() {
         $uid = Input::get('uid', '');
         if ($uid) {
             return Response::json(array('c' => 200, 'm' => 'OK', 'd' => array('profile' => User::find($uid)->toJson())));
