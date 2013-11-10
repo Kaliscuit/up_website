82add9b1de99e096de7ae77d22fa3cbd63210e51
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-10 15:24:17 +0800

    search

--- app/controllers/PositionController.php
+++ app/controllers/PositionController.php
@@ -31,9 +31,69 @@ class PositionController extends BaseController {
     }
 
     public function postHot() {
-        $xs     = new XS('zhaopin');
-        $search = $xs->search;
-        $keywords  = $search->getHotQuery(10);
-        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($keywords), 'keywords' => $keywords)));
+        $positions = [
+            [
+                'id'       => 6,
+                'rank'     => 1,
+                'trend'    => +3,
+                'position' => 'Android开发工程师'
+            ],
+            [
+                'id'       => 11,
+                'rank'     => 2,
+                'trend'    => -1,
+                'position' => 'PHP网站程序员'
+            ],
+            [
+                'id'       => 13,
+                'rank'     => 3,
+                'trend'    => +2,
+                'position' => 'UE/UI工程师'
+            ],
+            [
+                'id'       => 22,
+                'rank'     => 4,
+                'trend'    => +1,
+                'position' => '.NET程序员'
+            ],
+            [
+                'id'       => 27,
+                'rank'     => 5,
+                'trend'    => +1,
+                'position' => '产品调试'
+            ],
+            [
+                'id'       => 30,
+                'rank'     => 6,
+                'trend'    => -1,
+                'position' => 'C++软件开发工程师'
+            ],
+            [
+                'id'       => 5,
+                'rank'     => 7,
+                'trend'    => +2,
+                'position' => 'JAVA研发程序员'
+            ],
+            [
+                'id'       => 10,
+                'rank'     => 8,
+                'trend'    => +3,
+                'position' => 'AMX产品技术工程'
+            ],
+            [
+                'id'       => 2,
+                'rank'     => 9,
+                'trend'    => -1,
+                'position' => '高级软件工程师'
+            ],
+            [
+                'id'       => 15,
+                'rank'     => 10,
+                'trend'    => +4,
+                'position' => '自动化编程员'
+            ],
+        ];
+
+        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($positions), 'positions' => $positions)));
     }
 }
