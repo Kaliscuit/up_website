9f560f5796aeadeb1e4e19bbd5eed5cba5d508ab
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-10 14:00:50 +0800

    search

--- app/controllers/PositionController.php
+++ app/controllers/PositionController.php
@@ -30,4 +30,14 @@ class PositionController extends BaseController {
         return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($result), 'result' => $result)));
     }
 
+    public function anyPinyin() {
+
+//用法：
+//第二个参数留空则为gb1232编码
+        $a = Pinyin('PHP100中文网');
+//第二个参数随意设置则为utf-8编码
+        $b = Pinyin('张恩民', 1);
+        return $a . $b ;
+    }
+
 }
