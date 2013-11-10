a6a323d6d47ee40ef0a452210c824b75b9d76b7e
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-10 11:37:35 +0800

    search

--- app/controllers/PositionController.php
+++ app/controllers/PositionController.php
@@ -3,30 +3,13 @@ require '/srv/v2up.me/www/vendor/xunsearch/lib/XS.php';
 
 class PositionController extends BaseController {
 
-//    public function postSuggest() {
-//        $keyword    = Input::get('keyword', '');
-//        $xs         = new XS('zhaopin');
-//        $search     = $xs->search;
-//        $suggestion = $search->getExpandedQuery($keyword);
-//
-//        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($suggestion), 'positions' => $suggestion)));
-//    }
-
     public function postSuggest() {
-        $keyword = Input::get('keyword', '');
-        $xs      = new XS('zhaopin');
-        $search  = $xs->search;
-        $search->setLimit(5);
-        $docs   = $search->search('position:' . $keyword);
-        $result = [];
-        foreach ($docs as $doc) {
-            $result[] = [
-                'id'            => $doc->int,
-                'position'      => $doc->position
-            ];
-        }
+        $keyword    = Input::get('keyword', '');
+        $xs         = new XS('zhaopin');
+        $search     = $xs->search;
+        $suggestion = $search->getExpandedQuery($keyword);
 
-        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($result), 'result' => $result)));
+        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($suggestion), 'positions' => $suggestion)));
     }
 
     public function postSearch() {
