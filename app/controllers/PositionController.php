c0feb58f27355c8d8ed828d3a4b0c806c1bea2d8
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-10 11:29:13 +0800

    mail

--- app/controllers/PositionController.php
+++ app/controllers/PositionController.php
@@ -3,11 +3,20 @@ require '/srv/v2up.me/www/vendor/xunsearch/lib/XS.php';
 
 class PositionController extends BaseController {
 
+//    public function postSuggest() {
+//        $keyword    = Input::get('keyword', '');
+//        $xs         = new XS('zhaopin');
+//        $search     = $xs->search;
+//        $suggestion = $search->getExpandedQuery($keyword);
+//
+//        return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($suggestion), 'positions' => $suggestion)));
+//    }
+
     public function postSuggest() {
         $keyword    = Input::get('keyword', '');
         $xs         = new XS('zhaopin');
         $search     = $xs->search;
-        $suggestion = $search->getExpandedQuery($keyword);
+        $suggestion = $search->search('position:' . $keyword);
 
         return Response::json(array('c' => 200, 'm' => 'ok', 'd' => array('count' => count($suggestion), 'positions' => $suggestion)));
     }
