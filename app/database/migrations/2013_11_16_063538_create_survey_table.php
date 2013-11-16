624cd15eae1fc04e2fce690c0be7eff072c48a96
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-16 09:37:00 +0300

    add survey table

---
+++ app/database/migrations/2013_11_16_063538_create_survey_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
