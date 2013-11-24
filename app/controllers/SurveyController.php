e186c59ecc7cee53fcd005cba6f386011950c25b
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-24 20:18:40 +0800

    survey

--- app/controllers/SurveyController.php
+++ app/controllers/SurveyController.php
@@ -9,8 +9,22 @@ class SurveyController extends BaseController {
             $user_position = UserPosition::where('uid', '=', $uid)->get()->first();
 
             if ($user_position) {
-                $pid = $user_position->pid;
-//                return Response::json(array('c' => 200, 'm' => 'OK', 'd' => $data));
+                $pid       = $user_position->pid;
+                $pid       = 2;
+                $questions = Survey::where('pid', '=', $pid)->get()->first();
+                $survey    = [];
+                if ($questions) {
+                    foreach (range(1, 10) as $i) {
+                        $suffix   = sprintf("%02d", $i);
+                        $qid      = 'q_' . $suffix;
+                        $question = $questions->$qid;
+                        $survey[] = $question;
+                    }
+                    return Response::json(array('c' => 200, 'm' => 'OK', 'd' => ['survey' => $survey]));
+                } else {
+                    return Response::json(array('c' => 404, 'm' => 'No Survey For This Position Yet.'));
+                }
+
             } else {
                 return Response::json(array('c' => 405, 'm' => 'Select Position Required'));
             }
--- app/models/Survey.php
+++ app/models/Survey.php
@@ -1,1 +1,21 @@
 <?php
+
+/**
+ * Class Survey
+ *
+ * @property id               $id
+ * @property pid              $pid
+ * @property q_01             $q_01
+ * @property q_02             $q_02
+ * @property q_03             $q_03
+ * @property q_04             $q_04
+ * @property q_05             $q_05
+ * @property q_06             $q_06
+ * @property q_07             $q_07
+ * @property q_08             $q_08
+ * @property q_09             $q_09
+ * @property q_10             $q_10
+ */
+class Survey extends Eloquent {
+    protected $table = 'survey';
+}
