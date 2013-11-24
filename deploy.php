e5428eae9a52862c525b78e962d496a6ae5588fc
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-24 14:44:47 +0800

    deploy

--- deploy.php
+++ deploy.php
@@ -1,16 +1,16 @@
 <?php
 while (True) {
-	exec("git pull", $str1, $str2);
-	try {
-		if ($str1[0] == "Already up-to-date.") {
-			echo "Up to date.\n";
-		} else {
-			exec("git pull && /etc/init.d/php-fpm restart", $str1);
-			echo "Updated.\n";
-		}
-	} catch (Exception $e) {
-		exec("git pull && /etc/init.d/php-fpm restart", $str1);
-		echo "Updated.\n";
-	}
-	sleep(5);
+    exec("git pull", $str1, $str2);
+    try {
+        if ($str1[0] == "Already up-to-date.") {
+            echo "Up to date.\n";
+        } else {
+            exec("git pull && /etc/init.d/php-fpm restart", $str1);
+            echo "Updated.\n";
+        }
+    } catch (Exception $e) {
+        exec("git pull && /etc/init.d/php-fpm restart", $str1);
+        echo "Updated.\n";
+    }
+    sleep(5);
 }
