28d2909065f63171d495944223410191e6a65110
Yuhan Jian <yuhanjian@yahoo.com>
2013-11-24 20:36:05 +0800

    survey

--- app/controllers/SurveyController.php
+++ app/controllers/SurveyController.php
@@ -11,7 +11,7 @@ class SurveyController extends BaseController {
             if ($user_position) {
                 $pid       = $user_position->pid;
                 $pid       = 2;
-                $questions = Survey::where('pid', '=', $pid)->get(['id', 'question'])->first();
+                $questions = Survey::where('pid', '=', $pid)->get()->first();
                 $survey    = [];
                 if ($questions) {
                     foreach (range(1, 10) as $i) {
@@ -19,7 +19,7 @@ class SurveyController extends BaseController {
                         $q_suffix = 'q_' . $suffix;
                         $qid      = $questions->$q_suffix;
                         $question = [
-                            'question' => SurveyQuestion::find($qid)->toArray(),
+                            'question' => SurveyQuestion::find($qid)->get(['id', 'question'])->toArray(),
                             'options'  => SurveyOption::where('qid', '=', $qid)->get(['id', 'option', 'score', 'qid'])->toArray()
                         ];
                         $survey[] = $question;
